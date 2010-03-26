5e18304e345acc40d0d81f3a706ad7f880b54b02
Jurgen Bosmans <jurgen@2004.be>
2010-03-26 12:41:30 +0100

    removed default rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title
+    base_title = "Ruby on rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} ⎮ #{@title}"
+    end
+  end
 end
