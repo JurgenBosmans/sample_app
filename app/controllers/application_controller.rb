2b11816c8f803bb97df45ed66c15087d082510a3
Jurgen Bosmans <jurgen@2004.be>
2010-04-23 14:27:47 +0200

    foutje

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  include SessionHelper
+  include SessionsHelper
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
