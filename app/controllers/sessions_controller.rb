9980f10404af7b03b73145084f027ef16d69f9b3
Jurgen Bosmans <jurgen@2004.be>
2010-04-26 20:17:46 +0200

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-	user = User.authenticate(params[:session][:email], params[:session][:password])
+	user = User.authenticate(params[:session][:email], 
+							 params[:session][:password])
 	if user.nil? 
 		flash.now[:error] = "Invalid email/password combination." 
 		@title = "Sign in" 
@@ -19,5 +20,4 @@ class SessionsController < ApplicationController
 	sign_out
 	redirect_to root_path
   end
-
 end
